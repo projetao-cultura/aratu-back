30f53eb87d8f08992a25357760dafd69a5c1015a
Lucas Florêncio de Sousa <lfs@cin.ufpe.br>
2024-02-07 19:18:34 -0300

    db: ajustes para fazer as migrations automaticas

--- aratu-api/app/core/config.py
+++ aratu-api/app/core/config.py
@@ -3,7 +3,7 @@ from pydantic import BaseSettings
 class Settings(BaseSettings):
     APP_NAME: str = "Aratu API"
     DEBUG_MODE: bool = True
-    DATABASE_URL: str = "sqlite+aiosqlite:///./test.db"
+    DATABASE_URL: str = "sqlite:///database.db"
 
     class Config:
         env_file = ".env"
