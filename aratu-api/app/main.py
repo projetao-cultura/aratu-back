7b048a7e1e82e38e2b01f2de6f37d6f19960259a
Lucas Florêncio de Sousa <lfs@cin.ufpe.br>
2024-02-09 01:15:00 -0300

    fix: ajustes para deploy

--- aratu-api/app/main.py
+++ aratu-api/app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from starlette.responses import RedirectResponse
 from app.api.v1.usuario_router import usuario_router
 from app.api.v1.evento_router import evento_router
 
@@ -6,3 +7,7 @@ app = FastAPI()
 
 app.include_router(usuario_router, prefix="/usuarios", tags=["Usuários"])
 app.include_router(evento_router, prefix="/eventos", tags=["Eventos"])
+
+@app.get("/")
+def root():
+    return RedirectResponse(url="/docs")
