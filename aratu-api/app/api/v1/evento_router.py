38d519c0d0687d1756268acf2cbd3a86ad449959
Lucas Florêncio de Sousa <lfs@cin.ufpe.br>
2024-02-12 02:26:54 -0300

    feat: adicionado endpoint de feed paginado

--- aratu-api/app/api/v1/evento_router.py
+++ aratu-api/app/api/v1/evento_router.py
@@ -1,8 +1,9 @@
-from fastapi import APIRouter, HTTPException, Depends, status
+from math import ceil
+from fastapi import APIRouter, HTTPException, Depends, status, Query
 from sqlalchemy.orm import Session
 
 from app.models.models import Evento as ModelEvento 
-from app.schemas import Evento, EventoResponse
+from app.schemas import Evento, EventoList, EventoResponse, pagination_params, Pagination
 from app.db.base import get_db
 
 evento_router = APIRouter()
@@ -36,3 +37,31 @@ async def listar_eventos(db: Session = Depends(get_db)):
     eventos_response = [EventoResponse.from_orm(evento) for evento in eventos]
     
     return eventos_response
+
+
+@evento_router.get("/feed/", response_model=EventoList)
+async def feed_eventos(
+    db: Session = Depends(get_db),
+    page: int = Query(ge=1, default=1, description="Número da página para a paginação, começando de 1"),
+    perPage: int = Query(ge=1, le=100, default=10, description="Número de eventos por página"),
+    order: str = Query(default="asc", description="Ordenação dos eventos, 'asc' para ascendente e 'desc' para descendente")
+):
+    # Calcular o offset baseado na página atual
+    offset = (page - 1) * perPage
+
+    # Construir a query com ordenação e paginação
+    query = db.query(ModelEvento)
+    if order == "asc":
+        query = query.order_by(ModelEvento.data_hora.asc())
+    else:
+        query = query.order_by(ModelEvento.data_hora.desc())
+    eventos = query.offset(offset).limit(perPage).all()
+
+    # Criar a resposta dos eventos usando o esquema Pydantic
+    eventos_response = [EventoResponse.from_orm(evento) for evento in eventos]
+
+    # Calcular o total de páginas
+    total_eventos = db.query(ModelEvento).count()
+    total_pages = ceil(total_eventos / perPage)
+
+    return EventoList(pages=total_pages, eventos=eventos_response)
