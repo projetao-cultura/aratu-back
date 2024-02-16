b18ae90bf007b00b921d4f9bc678f0574ca15db6
Lucas Florêncio de Sousa <lfs@cin.ufpe.br>
2024-02-15 22:50:33 -0300

    docs: adicionado nome e versao da api

--- aratu-api/app/main.py
+++ aratu-api/app/main.py
@@ -3,7 +3,7 @@ from starlette.responses import RedirectResponse
 from app.api.v1.usuario_router import usuario_router
 from app.api.v1.evento_router import evento_router
 
-app = FastAPI()
+app = FastAPI(title="Aratu API", version="0.1")
 
 app.include_router(usuario_router, prefix="/usuarios", tags=["Usuários"])
 app.include_router(evento_router, prefix="/eventos", tags=["Eventos"])
