f95afaa06a6cdede3266b0b09dfbc8210f3084ea
Lucas Florêncio de Sousa <lfs@cin.ufpe.br>
2024-02-15 22:50:19 -0300

    feat: adicionada logica de autenticacao

--- aratu-api/app/api/v1/usuario_router.py
+++ aratu-api/app/api/v1/usuario_router.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter, HTTPException, status, Depends
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
+from app.core.auth import get_current_user, get_password_hash, create_access_token, verify_password
 from app.models.models import Usuario as ModelUsuario  
-from app.schemas import Usuario, UserResponse  
+from app.schemas import Usuario, UserResponse, Token
 from app.db.base import get_db
 
 usuario_router = APIRouter()
@@ -14,8 +16,10 @@ async def criar_usuario(usuario: Usuario, db: Session = Depends(get_db)):
     if db_usuario:
         raise HTTPException(status_code=400, detail="Email já está em uso")
     
+    hashed_password = get_password_hash(usuario.senha)
+    
     # Cria um novo usuário
-    novo_usuario = ModelUsuario(nome=usuario.nome, email=usuario.email, senha=usuario.senha, ativo=usuario.ativo)
+    novo_usuario = ModelUsuario(nome=usuario.nome, email=usuario.email, senha=hashed_password, ativo=usuario.ativo)
     db.add(novo_usuario)
     db.commit()
     db.refresh(novo_usuario)
@@ -30,4 +34,42 @@ async def buscar_usuario(usuario_id: int, db: Session = Depends(get_db)):
     if not usuario:
         raise HTTPException(status_code=404, detail="Usuário não encontrado")
     return UserResponse.from_orm(usuario)
+
+@usuario_router.put('/{usuario_id}', response_model=UserResponse, summary='Atualizar um Usuário')
+def update_user(
+    user_id: int,
+    user: Usuario,
+    session: Session = Depends(get_db),
+    current_user: ModelUsuario = Depends(get_current_user),
+):
+    if current_user.id != user_id:
+        raise HTTPException(status_code=400, detail='Not enough permissions')
+
+    current_user.email = user.email
+    current_user.senha = get_password_hash(user.senha)
+    session.commit()
+    session.refresh(current_user)
+
+    return current_user
+
+@usuario_router.post('/token', response_model=Token, summary='Gerar um Token de Acesso')
+def login_for_access_token(
+    form_data: OAuth2PasswordRequestForm = Depends(),
+    db: Session = Depends(get_db),
+):
+    usuario = db.query(ModelUsuario).filter(ModelUsuario.email == form_data.username).first()
+
+    if not usuario:
+        raise HTTPException(
+            status_code=400, detail='Incorrect email or password'
+        )
+
+    if not verify_password(form_data.password, usuario.senha):
+        raise HTTPException(
+            status_code=400, detail='Incorrect email or password'
+        )
+
+    access_token = create_access_token(data={'sub': usuario.email})
+
+    return {'access_token': access_token, 'token_type': 'bearer'}
 
