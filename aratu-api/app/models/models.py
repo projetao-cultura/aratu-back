576ee9719a04b94fb1b7ad4b3276c75bfcb4ae82
Lucas Florêncio de Sousa <lfs@cin.ufpe.br>
2024-02-17 18:54:25 -0300

    feat: adequando schema e model de evento

--- aratu-api/app/models/models.py
+++ aratu-api/app/models/models.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, String, Boolean, DateTime, Float, Enum
-from sqlalchemy.orm import declarative_base
+from sqlalchemy import Column, Integer, String, Boolean, DateTime, Float, Enum, ForeignKey
+from sqlalchemy.dialects.postgresql import ARRAY 
+from sqlalchemy.orm import declarative_base, relationship
 
 Base = declarative_base()
 
@@ -21,14 +22,31 @@ class Evento(Base):
     id = Column(Integer, primary_key=True, index=True)
     nome = Column(String)
     descricao = Column(String)
+    banner = Column(String) 
+    categoria = Column(ARRAY(String))
     local = Column(String)
     data_hora = Column(DateTime)
-    valor = Column(Float)
-    foto_url = Column(String)
-    likes = Column(Integer, default=0)
+    valor = Column(Float, default=0.0)
+    onde_comprar_ingressos = Column(String)
+    
+    avaliacoes = relationship("Avaliacao", back_populates="evento")
 
     def __repr__(self):
-        return f"<Evento(nome='{self.nome}', descricao='{self.descricao}', local='{self.local}', data_hora='{self.data_hora}', valor={self.valor}, foto_url='{self.foto_url}', likes={self.likes})>"
+        return f"<Evento(nome='{self.nome}', descricao='{self.descricao}', local='{self.local}', data_hora='{self.data_hora}', valor={self.valor}, banner='{self.banner}')>"
+    
+class Avaliacao(Base):
+    __tablename__ = "avaliacoes"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    evento_id = Column(Integer, ForeignKey('eventos.id'), nullable=False)
+    usuario_id = Column(Integer, ForeignKey('usuarios.id'), nullable=False)
+    avaliacao = Column(Integer)
+    
+    evento = relationship("Evento", back_populates="avaliacoes")
+    # Adicionaar a relação no modelo Usuario também, se necessário
+    
+    def __repr__(self):
+        return f"<Avaliacao(evento_id={self.evento_id}, usuario_id={self.usuario_id}, avaliacao={self.avaliacao})>"
 
 class ControleCarga(Base):
     __tablename__ = "controle_carga"
@@ -39,7 +57,7 @@ class ControleCarga(Base):
     fim_exec = Column(String)
     qtd_src_total = Column(Integer, default=0)
     qtd_sucesso = Column(Integer, default=0)
-    status = Column(Enum('ERRO', 'EM_PROGRESSO', 'SUCESSO'))
+    status = Column(Enum('ERRO', 'EM_PROGRESSO', 'SUCESSO', name="tipo_categoria"))
 
     def __repr__(self):
         return f"<ControleCarga(id={self.id}, fonte='{self.fonte}', inic_exec='{self.inic_exec}', fim_exec='{self.fim_exec}', qtd_src_total={self.qtd_src_total}, qtd_sucesso={self.qtd_sucesso}, status='{self.status}')>"
