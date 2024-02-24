81e0a6ecac51b8b189c64bca149f9168de6b2d5a
Lucas Florêncio de Sousa <lfs@cin.ufpe.br>
2024-02-24 12:34:47 -0300

    feat: adicionando rotas de add amigo e marcar eventos (fui e quero ir)

--- aratu-api/app/api/v1/usuario_router.py
+++ aratu-api/app/api/v1/usuario_router.py
@@ -8,6 +8,7 @@ from app.models.models import Usuario as ModelUsuario
 from app.schemas import UserResponse, UsuarioCreate, UsuarioUpdate, Token, EventoResponse
 from app.services.usuario_services import get_eventos_interesse
 from app.db.base import get_db
+import services.usuario_services as service
 
 usuario_router = APIRouter()
 
@@ -90,3 +91,18 @@ async def eventos_de_interesse_do_usuario(usuario_id: int, db: Session = Depends
         raise HTTPException(status_code=404, detail=str(e))
     
     return eventos_de_interesse
+
+@usuario_router.post("/{usuario_id}/amigos/{amigo_id}", status_code=status.HTTP_201_CREATED)
+async def adicionar_amigo(usuario_id: int, amigo_id: int, db: Session = Depends(get_db)):
+    service.adicionar_amigo(db, usuario_id, amigo_id)
+    return {"mensagem": "Amigo adicionado com sucesso"}
+
+@usuario_router.post("/{usuario_id}/quero_ir/{evento_id}", status_code=status.HTTP_201_CREATED)
+async def adicionar_evento_quero_ir(usuario_id: int, evento_id: int, db: Session = Depends(get_db)):
+    service.adicionar_evento_quero_ir(db, usuario_id, evento_id)
+    return {"mensagem": "Evento adicionado à lista de 'Quero Ir' com sucesso"}
+
+@usuario_router.post("/{usuario_id}/fui/{evento_id}", status_code=status.HTTP_201_CREATED)
+async def adicionar_evento_fui(usuario_id: int, evento_id: int, db: Session = Depends(get_db)):
+    service.adicionar_evento_fui(db, usuario_id, evento_id)
+    return {"mensagem": "Evento adicionado à lista de 'Fui' com sucesso"}
